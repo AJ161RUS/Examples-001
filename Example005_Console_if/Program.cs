9b8fd55e2f1e8ba1a59ccc595e70c2adc950e0f0
Andrey Zhulay <zhulay.00@mail.ru>
2023-03-07 19:40:15 +0300

    exmpl5

---
+++ Example005_Console_if/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username == "Виктория")
+{
+console.WriteLine("Ура, это Виктория");
+}
+else 
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
